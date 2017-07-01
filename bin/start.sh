5a81a9cd2eab79fb1ad4ebc67af26b961d7830e6
桑明明 <sam@172-0-14-188.lightspeed.rcsntx.sbcglobal.net>
2017-07-01 19:24:04 +0800

    update

--- bin/start.sh
+++ bin/start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-HUBOT_BEARYCHAT_TOKENS=704347a0c5ffd9ce2fb3d561403bd2b0 ~/My-Hubot/bin/hubot -a bearychat
+HUBOT_BEARYCHAT_TOKENS=704347a0c5ffd9ce2fb3d561403bd2b0 /var/data/MyHubot/bin/hubot -a bearychat
